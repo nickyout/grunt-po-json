b26b71e8b5784ce4e8f0c88310480da579de1f3f
Nicky Out <nickyout@gmail.com>
2014-03-02 16:46:54 +0100

    Forgot some files

---
+++ test/expected/custom_options.js
@@ -0,0 +1,1 @@
+define({"test":{"Single line id":"A single line string","Multi line id":"A multi line string","Another single line id":"A single line string 2"},"test2":{"Single line id, the second":"The second version of a line string","Multi line id 2":"The multi line string revisited","Yet another single line id":"A single line string 4"}});
